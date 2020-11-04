1bff815f9b60a623ca686d94497af1350f477940
tsuruta <tsuruta19951122@gmail.com>
2020-11-04 06:22:20 +0000

    kadai-tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -110,11 +110,10 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //
+        
         $task = Task::findOrFail($id);
-      
         $task->delete();
-
+    
         return redirect('/');
     }
 }
