fa8f58179c077448174f271be6bb1194f7246f9b
tsuruta <tsuruta19951122@gmail.com>
2020-11-29 15:36:14 +0000

    tasklist kadai 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
                         
                         <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>    
                             {{-- ログアウトへのリンク --}}
-                        <li class="nav-item">{!! link_to_route('logout.get', 'Logout') !!}</li> 
+                        <li class="nav-item">{!! link_to_route('logout.get', 'Logout', [], ['class' => 'nav-link']) !!}</li> 
                     </ul>
                         @else
                         {{-- ユーザ登録ページへのリンク --}}
